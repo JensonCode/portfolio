a4464e40c5591af49e2793fb3b4351069c852473
JensonCode <lclcoding0714@hotmail.com>
2024-09-15 17:12:04 -0400

    ops: github pages next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  //React Three Fiber
   transpilePackages: ['three'],
+
+  //GitHub Pages deploy config
+  reactStrictMode: true,
+  basePath: '/portfolio',
 };
 
 export default nextConfig;
