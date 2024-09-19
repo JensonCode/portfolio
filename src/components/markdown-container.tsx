976a73c628350bc93d4eddb4e11d5b67ab83fb7f
JensonCode <lclcoding0714@hotmail.com>
2024-09-19 11:23:13 -0400

    style: use app css in MarkdownCotainer

--- src/components/markdown-container.tsx
+++ src/components/markdown-container.tsx
@@ -9,6 +9,15 @@ export default function MarkdownContainer({ children }: MarkdownCotainer) {
   return (
     <ReactMarkdown
       components={{
+        h1: ({ node, ...props }) => (
+          <p className="mt-4 text-6xl font-bold drop-shadow" {...props} />
+        ),
+        h2: ({ node, ...props }) => (
+          <p className="mb-4 text-4xl font-bold drop-shadow" {...props} />
+        ),
+        h3: ({ node, ...props }) => (
+          <p className="mt-4 text-3xl font-bold drop-shadow-sm" {...props} />
+        ),
         p: ({ node, ...props }) => (
           <p
             className="my-4 bg-black/70 text-lg leading-relaxed text-muted-foreground"
